c6a319e59ed4623ccf9bd79bf47aaea569f7044c
punner <punpun.@gmail.com>
2024-11-23 16:41:40 +0700

    fix pew.py

--- pew.py
+++ pew.py
@@ -1,13 +1,13 @@
 a = float(input('Enter a = '))
 b = float(input('Enter b = '))
 c = float(input('Enter c = '))
-if a >> b:
+if a < b:
     S1 = a
     S2 = b
 else:
     S1 = b 
     S2 = a 
-if S2 == c:
+if S2 > c:
     S3 = S2
     S2 == c    
 else:
